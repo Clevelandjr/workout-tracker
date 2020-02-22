b1f3e670dbe74e0f4c37e24ff296ebdcdd441d00
Cleveland Pitt <cleveland.pitt@gmail.com>
2020-02-21 23:57:06 -0500

    added index and workout to models

---
+++ models/workout.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: 'Enter a name for workout'
+    },
+    type: {
+        type: String,
+        trim: true,
+        required: 'Enter a type of workout'
+        // resistance or cardio
+    },
+    weight: {
+        type: Number,
+        trim: true,
+        required: 'Enter the weight for the workout in pounds'
+    },
+    sets: {
+        type: String,
+        trim: true,
+        required: 'Enter the sets for the workout'
+    },
+    reps: {
+        type: String,
+        trim: true,
+        required: 'Enter the reps for the workout'
+    },
+    duration: {
+        type: Number,
+        trim: true,
+        required: 'Enter the duration for the workout in minutes'
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+}, {
+    toObject: {
+        virtuals: true
+    },
+    toJSON: {
+        virtuals: true 
+    }
+});
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout;
